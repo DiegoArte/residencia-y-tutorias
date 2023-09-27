054eecf3f42eb6d4d1f6c6ff4e463c0747d5fa3b
Barbarus1300 <fernanglezmaga@gmail.com>
2023-09-27 09:23:15 -0600

    .

--- VistaPsicologa1.php
+++ VistaPsicologa1.php
@@ -26,12 +26,13 @@
         </div>
         <table>
             <tr>
-                <td>Reporte</td>
-                <td>Nombre del alumno</td>
-                <td>Profesor que lo envia</td>
-                <td>Motivo</td>
-                <td>Responder</td>
+                <th>Reporte</th>
+                <th>Nombre del alumno</th>
+                <th>Profesor que lo envia</th>
+                <th>Motivo</th>
+                <th>Responder</th>
             </tr>
+        </table>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
