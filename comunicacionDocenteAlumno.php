28bfd1e5d555fafc34bf17b4fb24d710029cfbd2
DiegoArte <diegoarteag777@gmail.com>
2023-09-27 22:54:37 -0600

    a

--- comunicacionDocenteAlumno.php
+++ comunicacionDocenteAlumno.php
@@ -105,12 +105,8 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="js/comunicacionDocenteAlumno.js"></script>
     <script>
-        // Función para enviar el formulario cuando se hace clic en el botón
         document.getElementById('enviarFormulario').addEventListener('click', function() {
-            // Obtener el formulario
             var formulario = document.getElementsByClassName('checklist');
-            
-            // Enviar el formulario
             formulario.submit();
         });
     </script>
