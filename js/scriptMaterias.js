86a71dc0e75a4f3515867a5e997b5fdfdb01b434
DiegoArte <94568408+DiegoArte@users.noreply.github.com>
2023-10-10 08:45:54 -0600

    y

---
+++ js/scriptMaterias.js
@@ -0,0 +1,60 @@
+function mostrarArchivoSeleccionado(input) {
+    const button = input.parentElement;
+    if (input.files.length > 0) {
+        button.classList.add("archivo-seleccionado");
+    } else {
+        button.classList.remove("archivo-seleccionado");
+    }
+}
+
+function confirmarReemplazo() {
+    var respuesta = confirm("¿Desea reemplazar los datos existentes en la base de datos?");
+    if (respuesta === true) {
+        // Si el usuario confirma, establece el valor de un campo oculto para indicar el reemplazo
+        document.getElementById("reemplazar").value = "si";
+        // Envía el formulario
+        document.getElementById("form_excel").submit();
+    }
+}
+
+function mostrarBotonEliminar(id) {
+    var fila = document.getElementById("fila-" + id);
+    var botonEliminar = fila.querySelector("button.eliminar");
+    if (botonEliminar) {
+        botonEliminar.style.display = "block";
+    }
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+var filaIdEliminar; // Variable global para almacenar el ID de la fila que se va a eliminar
+
+function eliminarFila(id) {
+    filaIdEliminar = id;
+    // Muestra el modal
+    var modal = document.getElementById("eliminarModal");
+    modal.style.display = "block";
+}
+
+function confirmarEliminar() {
+    // Cierra el modal
+    var modal = document.getElementById("eliminarModal");
+    modal.style.display = "none";
+    
+    // Redirige para eliminar la fila
+    window.location.href = "php/eliminar_filaMat.php?id=" + filaIdEliminar;
+}
+
+function cancelarEliminar() {
+    // Cierra el modal
+    var modal = document.getElementById("eliminarModal");
+    modal.style.display = "none";
+}
+
+function mostrarErrorModal(mensaje) {
+    document.getElementById('errorMensaje').textContent = mensaje;
+    document.getElementById('errorModal').style.display = 'block';
+}
+
+function cerrarErrorModal() {
+    document.getElementById('errorModal').style.display = 'none';
+}
---
+++ php/eliminar_filaMat.php
@@ -0,0 +1,27 @@
+<?php
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    // Realiza la eliminación en la base de datos (reemplaza "nombre_de_la_tabla" con el nombre de tu tabla)
+    $mysqli = new mysqli("localhost", "root", "", "Tutorias_Residencia");
+    if (mysqli_connect_errno()) {
+        echo "Error de conexión a MySQL: " . mysqli_connect_error();
+        exit();
+    }
+
+    $deleteQuery = "DELETE FROM materias WHERE id = ?";
+    $stmt = $mysqli->prepare($deleteQuery);
+    $stmt->bind_param("i", $id);
+
+    if ($stmt->execute()) {
+        // Redirige de nuevo a la página principal o a donde desees después de eliminar
+        header("Location: ../RegistraMaterias.php");
+        exit();
+    } else {
+        echo "Error al eliminar fila: " . $stmt->error;
+    }
+
+    $stmt->close();
+    $mysqli->close();
+}
+?>
