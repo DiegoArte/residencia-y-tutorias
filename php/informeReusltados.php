442ea884324750b340342a01b76fd416627acf23
Barbarus1300 <fernanglezmaga@gmail.com>
2023-10-11 21:08:38 -0600

    .

--- php/informeReusltados.php
+++ php/informeReusltados.php
@@ -1,11 +1,7 @@
 <?php
-    require_once dirname(__FILE__).'../PHPWord-master/src/PhpWord/Autoloader.php';
-    \PhpOffice\PhpWord\Autoloader::register();
+    require_once '../phpoffice/vendor/autoload.php';
 
-
-    use PhpOffice\PhpWord\TemplateProcessor;
-
-    $plantillaInfResu=new TemplateProcessor('../Formatos/Informe_resultados.docx');
+    $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor('../Formatos/Informe_resultados.docx');
 
     $Tutor=$_POST["Tutor"];
     $jefAca=$_POST["jefAca"];
@@ -17,27 +13,27 @@
     $numDec=$_POST["numDec"];
     $lisNed=$_POST["lisNed"];
     $accRec=$_POST["accRec"];
-    $areApo=$_POST["areApop"];
+    $areApo=$_POST["areApo"];
     $numApro=$_POST["numApro"];
     $numRepro=$_POST["numRepro"];
     $infCom=$_POST["infCom"];
 
-    $templateWord-> setValue('Nobre_Tutor',$Tutor);
-    $templateWord-> setValue('Jefe_Div_Aca',$jefAca);
-    $templateWord-> setValue('Per_Sem',$pedSem);
-    $templateWord-> setValue('Grupo',$grupo);
-    $templateWord-> setValue('Prog_Est',$proEstu);
-    $templateWord-> setValue('Tus_Asig',$numTut);
-    $templateWord-> setValue('Num_Ent',$numEntr);
-    $templateWord-> setValue('Num_Ned',$numDec);
-    $templateWord-> setValue('Lis_Dec',$lisNed);
-    $templateWord-> setValue('Acc_Rec',$accRec);
-    $templateWord-> setValue('Are_Apo',$areApo);
-    $templateWord-> setValue('Num_Apro',$numApro);
-    $templateWord-> setValue('Num_Repro',$numRepro);
-    $templateWord-> setValue('Info_Comple',$infCom);
+    $templateProcessor-> setValue('Nobre_Tutor',$Tutor);
+    $templateProcessor-> setValue('Jefe_Div_Aca',$jefAca);
+    $templateProcessor-> setValue('Per_Sem',$pedSem);
+    $templateProcessor-> setValue('Grupo',$grupo);
+    $templateProcessor-> setValue('Prog_Est',$proEstu);
+    $templateProcessor-> setValue('Tus_Asig',$numTut);
+    $templateProcessor-> setValue('Num_Ent',$numEntr);
+    $templateProcessor-> setValue('Num_Ned',$numDec);
+    $templateProcessor-> setValue('Lis_Dec',$lisNed);
+    $templateProcessor-> setValue('Acc_Rec',$accRec);
+    $templateProcessor-> setValue('Are_Apo',$areApo);
+    $templateProcessor-> setValue('Num_Apro',$numApro);
+    $templateProcessor-> setValue('Num_Repro',$numRepro);
+    $templateProcessor-> setValue('Info_Comple',$infCom);
 
-    $templateWord->saveAs('Informe de Resultados'.date('d-m-y').'.docx');
+    $templateProcessor->saveAs('Informe de Resultados'.date('d-m-y').'.docx');
 
     header("Content-Disposition: attachment; filename=Informe de Resultados'.date('d-m-y').'.docx; charset=iso-8859-1");
     echo file_get_contents('Informe de Resultados'.date('d-m-y').'.docx');
