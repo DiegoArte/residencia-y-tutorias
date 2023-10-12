36a52a3f246d59a1adec4f9f18460c89032ab87b
Barbarus1300 <fernanglezmaga@gmail.com>
2023-10-11 18:20:19 -0600

    .

--- php/informeReusltados.php
+++ php/informeReusltados.php
@@ -1,3 +1,10 @@
 <?php
-    require_once dirname(__FILE__).'../PHPWord'
+    require_once dirname(__FILE__).'../PHPWord-master/src/PhpWord/Autoloader.php';
+    \PhpOffice\PhpWord\Autoloader::register();
+
+    use PhpOffice\PhpWord\TemplateProcessor;
+
+    $plantillaInfResu=new TemplateProcessor('../Formatos/Informe_resultados.docx');
+
+    
 ?>
