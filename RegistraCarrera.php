1c0bc707de036734ac3d1240aebea13de9fa1e2a
Barbarus1300 <fernanglezmaga@gmail.com>
2023-11-06 12:46:25 -0600

    .

--- RegistraCarrera.php
+++ RegistraCarrera.php
@@ -17,7 +17,8 @@ session_start();
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
 
@@ -100,7 +101,7 @@ session_start();
         </div>
     </div>
 
-    <button id="nuevoRegistroButton"  onclick="abrirFormularioRegistro()" >Nuevo Registro</button>
+    
 
     <!-- Modal de registro -->
     <div id="registro-modal" class="registro-modal modal" style="display: none;">
@@ -127,8 +128,14 @@ session_start();
     </div>
 
     <!-- Barra Serch -->
-    <input type="text" id="searchInput" placeholder="Buscar" onkeyup="search()">
-
+    <div class="row">
+        <div class="col">
+            <input type="text" id="searchInput" placeholder="Buscar" onkeyup="search()">
+        </div>
+        <div class="col">
+            <button id="nuevoRegistroButton"  onclick="abrirFormularioRegistro()" >Nuevo Registro</button>
+        </div>
+    </div>
 
     <?php
     require 'php/db.php';
