7abc3427f5d163dcca005e4bc5058d15a7f6d3d4
Alfonso758 <alfonsofrausto75@gmail.com>
2023-11-07 09:02:38 -0600

    estilo a recuperar contraseña

--- LoginResidencia.php
+++ LoginResidencia.php
@@ -76,6 +76,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/estiloLogin.css">
     <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/recupContra.css">
     <script src="js/scriptLogin.js"></script>
     <title>Login</title>
 </head>
