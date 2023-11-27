f161b3658e50ff8eef9228c86b0eb0cf17fb548a
Magalycs <lizgomez33@gmail.com>
2023-11-27 15:02:17 -0600

    fcgvhj

---
+++ indices/NUEVO.php
@@ -0,0 +1,169 @@
+<?php
+session_start();
+$carrera = $_GET['carrera'] ?? "";
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="indices.css">
+
+  <title>Informe de Índices</title>
+  
+</head>
+<body>
+  
+  <header class="fixed w-100">
+        <div class="usuarioOp d-flex justify-content-end">
+            <img src="profile.png" alt="">
+            <?php
+            $nombre = $_SESSION['nombre'];
+            echo '<p>' . $nombre . '</p>';
+            ?>
+             <div class="dropdown-content">
+                <a href="logout.php">Cerrar sesión</a>
+        </div>
+        </div>
+    </header>
+    <main>
+        <div class="barraLateral fixed h-100">
+            <a href="#"></a>
+
+            
+        </div>
+        <div class="container" style="margin-top: 10%;">
+          <h1>Informe de Índices</h1>
+          <?php
+            // Configuración de la conexión a la base de datos
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "tutorias_residencia";
+
+            // Crear conexión
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            // Verificar la conexión
+            if ($conn->connect_error) {
+              die("Conexión fallida: " . $conn->connect_error);
+            }
+
+            // Consulta SQL para obtener los datos
+            $sql = "SELECT carrera, materia, semestre, unidad, alumnosA, alumnosR FROM indices";
+
+            $result = $conn->query($sql);
+
+            // Comprobar si hay resultados y mostrar las gráficas por carrera y materia
+            if ($result->num_rows > 0) {
+              $chartData = array();
+
+              while ($row = $result->fetch_assoc()) {
+                // Ejemplo de variables con datos de la base de datos
+                $carrera = $row["carrera"];
+                $materia = $row["materia"];
+                $semestre= $row["semestre"];
+                $unidad = $row["unidad"];
+                $alumnosA = $row["alumnosA"];
+                $alumnosR = $row["alumnosR"];
+
+                // Calcular porcentajes
+                $porcentajeAlumnosA = ($alumnosA / ($alumnosA + $alumnosR)) * 100;
+                $porcentajeAlumnosR = ($alumnosR / ($alumnosA + $alumnosR)) * 100;
+
+                // Combinar gráficas si la carrera tiene el mismo nombre
+                if (!isset($chartData[$carrera][$materia])) {
+                  $chartData[$carrera][$materia] = array(
+                    'labels' => array(),
+                    'data' => array(),
+                  );
+                }
+
+                $chartData[$carrera][$materia]['labels'][] = 'Unidad ' . $unidad;
+
+                $chartData[$carrera][$materia]['data'][] = array(
+                    'Aprobados' => $alumnosA,
+                    'Reprobados' => $alumnosR,
+                    'PorcentajeAprobados' => $porcentajeAlumnosA,
+                    'PorcentajeReprobados' => $porcentajeAlumnosR
+                );
+              }
+
+              // Mostrar las gráficas
+              foreach ($chartData as $carrera => $materias) {
+                echo '<h2>' . $carrera . '</h2>';
+                foreach ($materias as $materia => $data) {
+                  echo '<div class="chart-container">';
+                  echo '<canvas class="bar-chart" data-carrera="' . $carrera . '" data-materia="' . $materia . '" data-semestre="' . $semestre .'" data-labels=\''
+                    . json_encode($data['labels']) . '\' data-data=\''
+                    . json_encode($data['data']) . '\'></canvas>';
+                  echo '</div>';
+                }
+              }
+            } else {
+              echo "No se encontraron datos en la tabla 'indices'.";
+            }
+
+            // Cerrar conexión
+            $conn->close();
+          ?>
+
+          <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+          <script>
+            // Configuración de las gráficas de barras
+            var charts = document.getElementsByClassName('bar-chart');
+
+            Array.from(charts).forEach(function(chart) {
+              var ctx = chart.getContext('2d');
+              var carrera = chart.getAttribute('data-carrera');
+              var materia = chart.getAttribute('data-materia');
+              var semestre= chart.getAttribute('data-semestre');
+              var labels = JSON.parse(chart.getAttribute('data-labels'));
+              var data = JSON.parse(chart.getAttribute('data-data'));
+
+              new Chart(ctx, {
+                type: 'bar',
+                data: {
+                  labels: labels,
+                  datasets: [{
+                    label: 'Alumnos Aprobados',
+                    data: data.map(item => item['PorcentajeAprobados']),
+                    backgroundColor: 'rgba(0, 128, 0, 0.7)', // Verde con opacidad
+                    borderWidth: 1
+                  }, {
+                    label: 'Alumnos Reprobados',
+                    data: data.map(item => item['PorcentajeReprobados']),
+                    backgroundColor: 'rgba(255, 0, 0, 0.7)', // Rojo con opacidad
+                    borderWidth: 1
+                  }]
+                },
+                options: {
+                  scales: {
+                    y: {
+                      beginAtZero: true,
+                      max: 100
+                    }
+                  },
+                  plugins: {
+                    title: {
+                      display: true,
+                      text:  carrera + ' - Materia: ' + materia + ' - Semestre: ' + semestre
+                    }
+                  }
+                }
+              });
+            });
+          </script>
+        </div>
+      </main>
+    
+  <a class="eliminar2" href="Tabla_mostrar.php">Regresar</a>
+
+</body>
+</html>
---
+++ indices/editar.php
@@ -0,0 +1,124 @@
+<?php
+    session_start();
+    $carrera=$_GET['carrera']??"";
+?>
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "tutorias_residencia";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Verificar la conexión
+    if ($conn->connect_error) {
+        die("Error de conexión: " . $conn->connect_error);
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["id"])) {
+        $id = $_GET["id"];
+
+        // Obtener datos del registro específico
+        $sql = "SELECT * FROM indices WHERE id=$id";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $carrera = $row["carrera"];
+            $materia = $row["materia"];
+            $semestre = $row["semestre"];
+            $unidad = $row["unidad"];
+            $alumnosA = $row["alumnosA"];
+            $alumnosR = $row["alumnosR"];
+        } else {
+            echo "Registro no encontrado";
+            exit();
+        }
+    } elseif ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["id"])) {
+        $id = $_POST["id"];
+        $carrera = $_POST["carrera"];
+        $materia = $_POST["materia"];
+        $semestre = $_POST["semestre"];
+        $unidad = $_POST["unidad"];
+        $alumnosA = $_POST["alumnosA"];
+        $alumnosR = $_POST["alumnosR"];
+
+        // Actualizar el registro en la base de datos
+        $sql = "UPDATE indices SET carrera='$carrera', materia='$materia', semestre='$semestre', unidad='$unidad', alumnosA='$alumnosA', alumnosR='$alumnosR' WHERE id=$id";
+
+        if ($conn->query($sql) === TRUE) {
+            header("Location: Tabla_mostrar.php");
+        } else {
+            echo "Error al actualizar el registro: " . $conn->error;
+        }
+    }
+
+    $conn->close();
+    ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="indices.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Editar Registro</title>
+</head>
+<body>
+
+
+<header class="fixed w-100">
+        <div class="usuarioOp d-flex justify-content-end">
+            <img src="profile.png" alt="">
+            <?php
+            $nombre = $_SESSION['nombre'];
+            echo '<p>' . $nombre . '</p>';
+            ?>
+           <div class="dropdown-content">
+                <a href="logout.php">Cerrar sesión</a>
+        </div>
+    </header>
+    <main>
+        <div class="barraLateral fixed h-100">
+            <a href="#"></a>
+        </div>
+        <div class="container" style="margin-top: 10%;">
+            <h1>Editar Registro</h1>
+
+            <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+                <input type="hidden" name="id" value="<?php echo $id; ?>">
+                <div class="col-md-5">
+                <label for="carrera" class="form-label">Plan de estudio</label>
+                <select class="form-select" id="carrera" name="carrera" required>
+                    <option value="0">Seleccione</option> 
+                    <option value="Ingeniería Industrial">Ingeniería Industrial</option>
+                    <option value="Ingeniería en Sistemas Computacionales">Ingeniería en Sistemas Computacionales</option>
+                    <option value="Ingeniería en Electromecánica">Ingeniería en Electromecánica</option>
+                    <option value="Ingeniería en Gestión Empresarial">Ingeniería en Gestión Empresarial</option>
+                    <option value="Contador Público">Contador Público</option>
+                    <option value="Ingeniería en Administración">Ingeniería en Administración</option>
+
+                </select>
+            </div>
+
+                Materia: <input type="text" name="materia" value="<?php echo $materia; ?>"><br>
+                Semestre: <input type="text" name="semestre" value="<?php echo $semestre; ?>"><br>
+                Unidad: <input type="text" name="unidad" value="<?php echo $unidad; ?>"><br>
+                AlumnosA: <input type="text" name="alumnosA" value="<?php echo $alumnosA; ?>"><br>
+                AlumnosR: <input type="text" name="alumnosR" value="<?php echo $alumnosR; ?>"><br>
+                <input class="eliminar" type="submit" value="Actualizar">
+            </form>
+
+        </div>
+    </main>
+        <a class="eliminar2" href="Tabla_mostrar.php" style="margin-top: 10%;">Regresar</a>
+
+
+</body>
+</html>
