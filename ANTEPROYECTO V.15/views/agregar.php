a6b5835b145f40cf8915a1579d2b720742835952
DiegoArte <94568408+DiegoArte@users.noreply.github.com>
2023-12-05 21:47:39 -0600

    ji

--- ANTEPROYECTO V.15/views/agregar.php
+++ ANTEPROYECTO V.15/views/agregar.php
@@ -50,20 +50,6 @@
                     </div>
 
 
-                    <div class="col-sm-6">
-                        <div class="mb-3">
-                            <label for="carrera" class="form-label">Selecciona una carrera</label>
-                            <select id="carrera" name="carrera" class="form-control">
-                                <?php while($carrera = mysqli_fetch_assoc($consulta_carreras)) { ?>
-                                    <option value="<?php echo $carrera['NombredeCarrera']; ?>">
-                                        <?php echo $carrera['NombredeCarrera']; ?>
-                                    </option>
-                                <?php } ?>
-                            </select>
-                        </div>
-                    </div>
-
-
 
                     <div class="col-12">
                         <label for="yourPassword" class="form-label">Archivo (PDF)</label>
